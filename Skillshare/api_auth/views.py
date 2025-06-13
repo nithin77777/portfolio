c1f251734fc5e3dfc91db835a2b874d090f985a2
nithin77777 <55943967+nithin77777@users.noreply.github.com>
2025-06-13 13:07:17 +0530

    app auth Files

--- Skillshare/api_auth/views.py
+++ Skillshare/api_auth/views.py
@@ -1,3 +1,15 @@
-from django.shortcuts import render
-
+from django.shortcuts import render, redirect
+from django.http import HttpResponse
+from rest_framework import status
+from rest_framework.views import APIView
 # Create your views here.
+def index(req):
+    """
+    Render the index page.
+
+    
+    """
+   
+    return HttpResponse(
+                         """<h1>Hello, world! This is the index page. </h1>"""
+                         )
