e84a26caeb2d96cd798dd0dd28a0b728a4446b5b
nithin77777 <55943967+nithin77777@users.noreply.github.com>
2025-06-13 16:01:46 +0530

    created User -> Model

--- Skillshare/api_auth/models.py
+++ Skillshare/api_auth/models.py
@@ -1,3 +1,21 @@
 from django.db import models
 
 # Create your models here.
+
+class User(models.Model):
+    """
+    Represents a user in the Skillshare application.
+    
+    Attributes:
+        username (str): The username of the user.
+        email (str): The email address of the user.
+        password (str): The password of the user.
+    """
+    username = models.CharField(max_length=150, unique=True)
+    email = models.EmailField(unique=True)
+    password = models.CharField(max_length=128)
+
+    def __str__(self):
+        return str(self.username) + "is the created username"
+    
+    
