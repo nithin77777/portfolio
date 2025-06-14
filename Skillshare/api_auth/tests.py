f05baac98b39f7e9fed6715d598ac1298cc7c65e
nithin77777 <55943967+nithin77777@users.noreply.github.com>
2025-06-14 17:29:05 +0530

    Tests For Login

--- Skillshare/api_auth/tests.py
+++ Skillshare/api_auth/tests.py
@@ -1,3 +1,32 @@
 from django.test import TestCase
+from django.urls import reverse
+from .models import User
+# from .models import User  # Use your custom User model
+from django.contrib.auth.hashers import make_password
+
 
 # Create your tests here.
+class  UserLoginTests(TestCase):
+    def setUp(self):
+        self.url = reverse('login')  # Make sure your login URL is named 'login'
+        # Create a test user
+        self.user = User.objects.create(
+            username='test',
+            email='test@example.com',
+            password=make_password('root1234')
+        )
+    
+    def test_valid_login(self):
+        response = self.client.post(self.url, {
+            'username': 'test',
+            'password': 'root1234'
+        })
+        self.assertRedirects(response, reverse('success_view'))
+
+    def test_invalid_password(self):
+        response = self.client.post(self.url, {
+            'username': 'test',
+            'password': 'wrongpass',
+            
+        })
+        self.assertContains(response, "Invalid Password")
