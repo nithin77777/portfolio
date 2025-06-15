ffce92bc60f0f8610bb7b6fe3bc7a6ec50d751c1
nithin77777 <55943967+nithin77777@users.noreply.github.com>
2025-06-15 14:22:02 +0530

    postgresql connection added

--- Skillshare/Skillshare/settings.py
+++ Skillshare/Skillshare/settings.py
@@ -80,10 +80,20 @@ WSGI_APPLICATION = 'Skillshare.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'testdb',  # Replace with your database name
+        'USER': 'iamleading',  # Replace with your database user
+        'PASSWORD': 'mnsk',  # Replace with your database password
+        'HOST': 'localhost',  # Replace with your database host
+        'PORT': '5432',  # Default PostgreSQL port
     }
 }
 
