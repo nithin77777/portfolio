8ed46e87c0f78e21966bd9bcb207cfa6b92950d9
nithin77777 <55943967+nithin77777@users.noreply.github.com>
2025-06-16 09:19:52 +0530

    Services App Related

--- Skillshare/servicesApp/urls.py
+++ Skillshare/servicesApp/urls.py
@@ -14,11 +14,13 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.contrib import admin
-from django.urls import path, include
+
+from django.urls import path
+
+from .views import ServiceListView
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('auth/', include('api_auth.urls')),
-    path('services/', include('api_auth.urls')),
+    
+   
+    path('all/',ServiceListView.as_view(), name='services'),
 ]
--- Skillshare/servicesApp/views.py
+++ Skillshare/servicesApp/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
 
+from django.http import HttpResponse
+from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
+from django.urls import reverse_lazy
+
+from .models import Service
 # Create your views here.
+
+class ServiceListView(ListView):
+    model = Service
+    template_name = 'servicesApp/service_list.html'
+    context_object_name = 'services'
+
+    
+
