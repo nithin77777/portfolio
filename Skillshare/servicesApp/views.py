922fb736097c3eb5ef4e3b14c3adc4d3573abbe6
nithin77777 <55943967+nithin77777@users.noreply.github.com>
2025-06-17 18:13:42 +0530

    pk based view added

--- Skillshare/servicesApp/views.py
+++ Skillshare/servicesApp/views.py
@@ -3,6 +3,7 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
 from django.urls import reverse_lazy
+from django.contrib.auth.decorators import login_required
 
 from .models import Service
 # Create your views here.
@@ -13,4 +14,13 @@ class ServiceListView(ListView):
     context_object_name = 'services'
 
     
-
+class ServiceDetailView(DetailView):
+    '''
+    View to display the details of a specific service.
+    It inherits from DetailView and uses the Service model.
+    The template used is 'servicesApp/service_detail.html'.
+    The service object uses pk to display each service'.
+    '''
+    model = Service
+    template_name = 'servicesApp/service_details.html'
+    context_object_name = 'service'
