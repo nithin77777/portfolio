07d267faa1df8df764083cced82275219504ebce
nithin77777 <55943967+nithin77777@users.noreply.github.com>
2025-06-18 12:30:17 +0530

    Bookings model added

--- Skillshare/servicesApp/models.py
+++ Skillshare/servicesApp/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+# models from other apps
 from api_auth.models import User
 # Create your models here.
 
@@ -11,3 +12,12 @@ class Service(models.Model):
 
     def __str__(self):
         return str(self.service_name)
+
+# Booking model to handle bookings for services
+class Booking(models.Model):
+    username = models.ForeignKey(User, on_delete=models.CASCADE, related_name='bookings')
+    service_booked = models.ForeignKey(Service, on_delete=models.CASCADE, related_name='bookings')
+    booking_date = models.DateField(auto_now_add=True, blank=True, null=True)
+
+    def __str__(self):
+        return str(self.username) + ' booked' + str(self.service_booked) + ' on ' + str(self.booking_date)
