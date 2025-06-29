698446aaae78afcddf9f5a9c8cc84651e44e7d8a
nithin77777 <55943967+nithin77777@users.noreply.github.com>
2025-06-29 13:46:40 +0530

    removed username

--- Skillshare/servicesApp/forms.py
+++ Skillshare/servicesApp/forms.py
@@ -10,7 +10,7 @@ It inherits from forms.ModelForm and is bound to the Booking model.
 class BookingForm(forms.ModelForm):
     class Meta:
         model = Booking
-        fields = ['username', 'service_booked', 'booking_date']
+        fields = ['service_booked', 'booking_date']
         widgets = {
             'service_booked': forms.Select(attrs={'class': 'form-control'}),
             'booking_date': forms.DateInput(attrs={'type': 'date', 'class': 'form-control'}),
